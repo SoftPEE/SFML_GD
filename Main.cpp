7bb5b8e00febcd2b01cbcc0d7a894de5af02c6f0
gehrt <gehrt@DESKTOP-Q98HU5F>
2017-02-08 15:51:28 +0100

    Projektdateien hinzufügen.

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+/****************************************************************************************************************************
+SFML_GD
+Author:				Patrick Gehrt
+Datum:				08.02.2017
+Version:			0.0.0.1
+SFML-Version:	2.4.1
+
+Beschreibung:	SFML Game Development
+	Lernen wie mit Hilfe der SFML-Bibliothek und C++ ein Spiel entwickelt wird.
+
+****************************************************************************************************************************/
+
+
+
+#include <iostream>
+
+
+int main()
+{
+	
+
+
+	return EXIT_SUCCESS;
+}
