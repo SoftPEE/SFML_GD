dc56b4afb8f8dcf60fed34e4ce26d88b0dd15795
Home <gehrt.patrick@outlook.com>
2017-02-08 16:37:32 +0100

    Grundgerüst für die Bewegungssteuerung hinzugefügt

--- Headers/Game.hpp
+++ Headers/Game.hpp
@@ -14,8 +14,15 @@ private:
   void update( );
   void render( );
 
+  void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
 private:
   sf::RenderWindow mWindow;
   sf::CircleShape  mPlayer;
+
+  bool             mIsMovingUp;
+  bool             mIsMovingDown;
+  bool             mIsMovingLeft;
+  bool             mIsMovingRight;
 };
 
--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -3,6 +3,10 @@
 Game::Game( )
   : mWindow{ sf::VideoMode(640,480), "SFML Application" }
   , mPlayer{ }
+  , mIsMovingUp{false }
+  , mIsMovingDown{false }
+  , mIsMovingLeft{false }
+  , mIsMovingRight{false }
 {
   mPlayer.setRadius(40.f);
   mPlayer.setPosition(100.f, 100.f);
@@ -33,14 +37,34 @@ void Game::processEvents( )
 
   while (mWindow.pollEvent(event))
   {
-    if (event.type == sf::Event::Closed)
-      mWindow.close();
+    switch (event.type)
+    {
+      case sf::Event::KeyPressed: 
+        handlePlayerInput(event.key.code, true); 
+        break;
+      case sf::Event::KeyReleased: 
+        handlePlayerInput(event.key.code, false);
+        break;
+      case sf::Event::Closed:
+        mWindow.close();
+    }
   }
 }
 
 
 void Game::update( )
 {
+  sf::Vector2f movement(0.f,0.f);
+  if (mIsMovingUp)
+    movement.y -= 1.0;
+  if (mIsMovingDown)
+    movement.y += 1.0;
+  if (mIsMovingLeft)
+    movement.x -= 1.0;
+  if (mIsMovingRight)
+    movement.x += 1.0;
+
+  mPlayer.move(movement);
 }
 
 
@@ -49,4 +73,16 @@ void Game::render( )
   mWindow.clear();
   mWindow.draw(mPlayer);
   mWindow.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+  if (key == sf::Keyboard::W)
+    mIsMovingUp = isPressed;
+  else if (key == sf::Keyboard::S)
+    mIsMovingDown = isPressed;
+  else if (key == sf::Keyboard::A)
+    mIsMovingLeft = isPressed;
+  else if (key == sf::Keyboard::D)
+    mIsMovingRight = isPressed;
 }
